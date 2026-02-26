975391d9b21cf7558d4247b2145b2c7ba98f5111
Vibisha.V.P <vibisharaveendran0@gmail.com>
2026-02-26 14:39:25 +0530

    upload config.py

---
+++ GEN AI Projects/career-advisor-chatbot/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GOOGLE_API_KEY not found in .env file")
